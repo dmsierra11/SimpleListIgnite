2a948de183b43ff3a161aa35a044ec6cec44c717
Daniel Sierra  F <danieluchin@gmail.com>
2018-09-13 19:41:13 +0200

    Added RatesList

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import RatesList from '../Containers/RatesList'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  RatesList: { screen: RatesList },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'RatesList',
   navigationOptions: {
     headerStyle: styles.header
   }
