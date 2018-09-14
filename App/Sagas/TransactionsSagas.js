2434b7031e45823b5fff5d6a1353834d42604290
Daniel Sierra  F <danieluchin@gmail.com>
2018-09-14 12:08:11 +0200

    Converted (only when there is the direct conversion)

--- App/Sagas/TransactionsSagas.js
+++ App/Sagas/TransactionsSagas.js
@@ -14,6 +14,7 @@ import { call, put, select } from 'redux-saga/effects'
 import TransactionsActions, { TransactionsSelectors } from '../Redux/TransactionsRedux'
 
 const selectCurrentData = (state) => state.transactions.data
+const selectCurrentRates = (state) => state.rates.data
 
 export function* getTransactions(api, action) {
   const { data } = action
@@ -26,7 +27,6 @@ export function* getTransactions(api, action) {
   if (response.ok) {
     // You might need to change the response here - do this with a 'transform',
     // located in ../Transforms/. Otherwise, just pass the data back from the api.
-    console.log("TRANSACTIONS: "+JSON.stringify(response.data))
     yield put(TransactionsActions.transactionsSuccess(response.data))
   } else {
     yield put(TransactionsActions.transactionsFailure())
@@ -39,23 +39,48 @@ export function* getTotalTransactions(action) {
   const currentData = yield select(selectCurrentData)
   let amounts_to_sum = []
   let item_found = false
+  let currency = ''
   for (index = 0; index < currentData.length; index++){
     let item = currentData[index]
-    if (item.sku = sku && !item_found) {
+    if (item.sku === sku && !item_found) {
+      currency = item.currency
       item_found = true
-      console.log("Go through the whole array to find index")
       for (i = index; i < currentData.length; i++) {
-        if (currentData[i].sku === item.sku) {
-          amounts_to_sum.push(currentData[i].amount)
+        currentItem = currentData[i]
+        if (currentItem.sku === item.sku) {
+          let amount = currentItem.amount
+          if (currentItem.currency !== currency)
+            amount = yield call(calculateRate, currentItem.amount, currency, currentItem.currency)
+          amounts_to_sum.push(amount)
         }
       }
+      yield call(sumValues, amounts_to_sum, currency)
     }
   }
-  yield call(sumValues, amounts_to_sum)
 }
 
-function* sumValues(values) {
-  console.log("VALUES: "+JSON.stringify(values))
+function* calculateRate(amount, from, to){
+  console.log("CONVERTING FROM "+from+" TO "+to)
+  let rates_avail = []
+  const currentRates = yield select(selectCurrentRates)
+  console.log("RATES: "+JSON.stringify(currentRates))
+  for (i = 0; i < currentRates.length ; i++){
+    let current = currentRates[i]
+    if (current.from === from){
+      if (current.to === to){
+        console.log("AMOUNT: "+amount)
+        console.log("RATE: "+current.rate)
+        return (parseFloat(amount)/parseFloat(current.rate)).toFixed(2)
+      }
+      else rates_avail.push(current.to)
+    }
+  }
+  console.log("RATES AVAILABLE: "+JSON.stringify(rates_avail))
+  return -1
+}
+
+function* sumValues(values, currency) {
+  console.log("Adding values: "+JSON.stringify(values))
   let total = 0
   for (i = 0; i < values.length; i++) {
     total += parseFloat(values[i])
